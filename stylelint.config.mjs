281727ab9dab0c47f9db8b493a7410b448f16b67
arthur <plazanet.art@gmail.com>
2026-02-21 17:46:52 +0100

    Install Prettier, Eslint and Stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-html",
+    "stylelint-config-html/vue",
+    "stylelint-config-html/astro",
+  ],
+};
