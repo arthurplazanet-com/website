717d50bf5ee89c8df34d09f50132f53c6a482f7e
arthur <plazanet.art@gmail.com>
2026-02-21 17:20:15 +0100

    Update to add projects to page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,23 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
-// https://astro.build/config
+// https://astro.build/config 
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+
+   vite: {
+    // publicDir: 'public',
+    build: {
+      rollupOptions: {
+        external: ['@use-compose/ui', '@use-compose/ui/dist/*'],
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
+        },
+        // external: ['@use-compose/ui', '@use-compose/ui/style.css'],
+        // , '@use-compose/ui'],
+        // noExternal: [/\.css$/],
+      },
+    },
 });
